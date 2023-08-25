ae5d0a3624f819f65dadfe0cd432e6e6304f17e3
Sagar Mandal <69377939+mandalsagar228@users.noreply.github.com>
2023-08-25 19:01:37 +0545

    backend url added

--- client/src/Components/Services/api.js
+++ client/src/Components/Services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:8000";
+const URL = "https://complete-authentication-system.onrender.com";
 
 // Sending signup data to the server.
 export const sendingSignupData = async (signupData) => {
